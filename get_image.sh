2464c4c8236dec84dd9b505b545f900631bebf3b
Craig Schardt <crschardt@fastem.com>
2026-04-25 16:38:54 -0500

    add cache action

--- get_image.sh
+++ get_image.sh
@@ -3,9 +3,7 @@ set -euxo pipefail
 shopt -s extglob
 
 url=$1
-download_path="${RUNNER_TEMP}/image"
-echo "download_path=${download_path}" >> "$GITHUB_ENV"
-mkdir --parent "${download_path}"
+cached=$2
 
 image=""
 
@@ -15,26 +13,32 @@ case ${url} in
         image="${url#file://}"
     ;;
     http?(s)://*.yam?(l) )
-        apt-get --quiet update
-        apt-get --yes --quiet install yq
-        echo "Downloading manifest from ${url}"
-        wget --no-verbose --output-document="manifest.yaml" "${url}"
-        echo "=== Manifest contents ==="
-        cat manifest.yaml
-        echo "========================="
-        yq -r '.urls[] | "\(.url) \(.sha256sum)"' ./manifest.yaml > urls
-        while read -r file_url sha; do
-            filename="$(basename ${file_url})"
-            echo "Downloading: ${filename} from ${file_url}"
-            wget --no-verbose --output-document=${download_path}/${filename} ${file_url}
-            echo "$sha ${download_path}/$filename" | sha256sum -c -
-            [[ ${filename} = *.img.xz ]] && image="${download_path}/${filename}"
-        done < urls
+        if [[ ${cached} == 'true' ]]; then
+            image=find ${image_path} -name *.img* -printf "%s %p\n" | sort -n | tail -1 | cut -d " " -f2
+        else
+            apt-get --quiet update
+            apt-get --yes --quiet install yq
+            echo "Downloading manifest from ${url}"
+            wget --no-verbose --output-document="manifest.yaml" "${url}"
+            echo "=== Manifest contents ==="
+            cat manifest.yaml
+            echo "========================="
+            yq -r '.urls[] | "\(.url) \(.sha256sum)"' ./manifest.yaml > urls
+            while read -r file_url sha; do
+                filename="$(basename ${file_url})"
+                echo "Downloading: ${filename} from ${file_url}"
+                wget --no-verbose --output-document=${image_path}/${filename} ${file_url}
+                echo "$sha ${image_path}/$filename" | sha256sum -c -
+                [[ ${filename} = *.img.xz ]] && image="${image_path}/${filename}"
+            done < urls
+        fi
     ;;
     http?(s)://* )
-        image="${download_path}/$(basename ${url})"
-        echo "Downloading ${image} from ${url}"
-        wget --no-verbose --output-document="${image}" ${url}
+        image="${image_path}/$(basename ${url})"
+        if [[ ${cached} != 'true' ]]; then
+            echo "Downloading ${image} from ${url}"
+            wget --no-verbose --output-document="${image}" ${url}
+        fi
     ;;
     * )
         echo "Unrecognized image source ${url}. Exiting!"
@@ -43,7 +47,7 @@ case ${url} in
 esac
 
 echo "Image: ${image}"
-ls -la ${download_path}
+ls -la ${image_path}
 
 if [[ ${image} = *.xz ]]; then
     echo "Unzipping ${image}"
