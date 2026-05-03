35e8e0ed7674934d05829598e30a0df90a05c84f
Craig Schardt <crschardt@fastem.com>
2026-05-03 13:56:17 -0500

    Add caching for images (#7)

--- get_image.sh
+++ get_image.sh
@@ -3,38 +3,47 @@ set -euxo pipefail
 shopt -s extglob
 
 url=$1
-download_path="${RUNNER_TEMP}/image"
-echo "download_path=${download_path}" >> "$GITHUB_ENV"
-mkdir --parent "${download_path}"
+cached=$2
 
 image=""
+image_path="${RUNNER_TEMP}/image"
+mkdir --parent "${image_path}"
 
 case ${url} in
     file://* )
-        echo "Using local file as image: ${url}"
         image="${url#file://}"
+        image_path="$(dirname ${image})"
+        echo "Using local file as image: ${url}"
     ;;
     http?(s)://*.yam?(l) )
-        apt-get --quiet update
-        apt-get --yes --quiet install yq
-        echo "Downloading manifest from ${url}"
-        wget --no-verbose --output-document="manifest.yaml" "${url}"
-        echo "=== Manifest contents ==="
-        cat manifest.yaml
-        echo "========================="
-        yq -r '.urls[] | "\(.url) \(.sha256sum)"' ./manifest.yaml > urls
-        while read -r file_url sha; do
-            filename="$(basename ${file_url})"
-            echo "Downloading: ${filename} from ${file_url}"
-            wget --no-verbose --output-document=${download_path}/${filename} ${file_url}
-            echo "$sha ${download_path}/$filename" | sha256sum -c -
-            [[ ${filename} = *.img.xz ]] && image="${download_path}/${filename}"
-        done < urls
+        if [[ ${cached} != 'true' ]]; then
+            sudo apt-get --quiet update
+            sudo apt-get --yes --quiet install yq
+            echo "Downloading manifest from ${url}"
+            wget --no-verbose --output-document="manifest.yaml" "${url}"
+            echo "=== Manifest contents ==="
+            cat manifest.yaml
+            echo "========================="
+            yq -r '.urls[] | "\(.url) \(.sha256sum)"' ./manifest.yaml > urls
+            while read -r file_url sha; do
+                filename="$(basename ${file_url})"
+                echo "Downloading: ${filename} from ${file_url}"
+                wget --no-verbose --output-document=${download_path}/${filename} ${file_url}
+                echo "$sha ${download_path}/$filename" | sha256sum -c -
+                [[ ${filename} = *.img.xz ]] && image="${download_path}/${filename}"
+            done < urls
+        fi
+        cp -r "${download_path}/." "${image_path}/"
+        image=$(find ${image_path} -type f \( -name *.img* \) -printf "%s %p\n" | sort -n | tail -1 | cut -d " " -f2)
     ;;
     http?(s)://* )
-        image="${download_path}/$(basename ${url})"
-        echo "Downloading ${image} from ${url}"
-        wget --no-verbose --output-document="${image}" ${url}
+        if [[ ${cached} != 'true' ]]; then
+            download="${download_path}/$(basename ${url})"
+            echo "Downloading from ${url} to ${download}"
+            wget --no-verbose --output-document="${download}" ${url}
+        fi
+        cp -r "${download_path}/." "${image_path}/"
+        image=${image_path}/$(basename ${url})
     ;;
     * )
         echo "Unrecognized image source ${url}. Exiting!"
@@ -43,7 +52,7 @@ case ${url} in
 esac
 
 echo "Image: ${image}"
-ls -la ${download_path}
+ls -la ${image_path}
 
 if [[ ${image} = *.xz ]]; then
     echo "Unzipping ${image}"
@@ -53,9 +62,9 @@ fi
 
 if [[ ${image} = *.tar ]]; then
     echo "Untarring ${image}"
-    tar -xf ${image}
+    tar -C ${image_path} -xf ${image}
     rm ${image}
-    image=$(find . -type f \( -name '*.img' \) -exec ls -s {} + 2>/dev/null | sort -rn | head -n1 | awk '{print $2}')
+    image=$(find ${image_path} -type f \( -name *.img \) -printf "%s %p\n" | sort -n | tail -1 | cut -d " " -f2)
 fi
 
 echo "Image: ${image}"
@@ -66,4 +75,5 @@ if [[ ${image} != *.img ]]; then
     exit 1
 fi
 
+echo "image_path=${image_path}" >> "$GITHUB_ENV"
 echo "image=${image}" >> "$GITHUB_OUTPUT"
